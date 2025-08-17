4077602d0b56aa6f29fa65f365124a52460fd481
Laabh Gupta <reachlaabhgupta@gmail.com>
2025-08-17 22:25:22 +0530

    Initial Phase

---
+++ src/main/java/net/LaabhGupta/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,41 @@
+package net.LaabhGupta.journalApp.controller;
+
+import net.LaabhGupta.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    public Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteEntryById(@PathVariable Long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("id/{id}")
+    public JournalEntry updateJournalById(@PathVariable Long id, @RequestBody JournalEntry myEntry){
+        return journalEntries.put(id, myEntry);
+    }
+}
