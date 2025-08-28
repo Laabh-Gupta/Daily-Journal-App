6a85f941f186cf163703de694db94d45774bb79f
Laabh Gupta <reachlaabhgupta@gmail.com>
2025-08-28 16:25:18 +0530

    chore: add Lombok for boilerplate reduction

--- src/main/java/net/LaabhGupta/journalApp/entity/JournalEntry.java
+++ src/main/java/net/LaabhGupta/journalApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package net.LaabhGupta.journalApp.entity;
 
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,41 +10,11 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
     private String title;
     private String content;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
     private LocalDateTime date;
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
