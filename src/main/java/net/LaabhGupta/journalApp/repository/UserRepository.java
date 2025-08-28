f78055ff1ec8481c6020b449ab814caba5b6d3ce
Laabh Gupta <reachlaabhgupta@gmail.com>
2025-08-28 18:47:09 +0530

    build: Added user creation and deletion

---
+++ src/main/java/net/LaabhGupta/journalApp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package net.LaabhGupta.journalApp.repository;
+
+import net.LaabhGupta.journalApp.entity.JournalEntry;
+import net.LaabhGupta.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String username);
+}
--- src/main/java/net/LaabhGupta/journalApp/service/JournalEntryService.java
+++ src/main/java/net/LaabhGupta/journalApp/service/JournalEntryService.java
@@ -1,21 +1,31 @@
 package net.LaabhGupta.journalApp.service;
 
+import lombok.extern.slf4j.Slf4j;
 import net.LaabhGupta.journalApp.entity.JournalEntry;
 import net.LaabhGupta.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 
 @Component
+@Slf4j
 public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
 
     public void saveEntry(JournalEntry journalEntry){
+        try{
+            journalEntry.setDate(LocalDateTime.now());
+            journalEntryRepository.save(journalEntry);
+        }
+        catch (Exception e){
+            log.error("Exception ", e);
+        }
         journalEntryRepository.save(journalEntry);
     }
 
