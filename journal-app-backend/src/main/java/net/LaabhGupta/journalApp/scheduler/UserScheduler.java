df1a781db39f3f81f4a3cf5495418991b4aa65a5
Laabh-Gupta <127779409+Laabh-Gupta@users.noreply.github.com>
2025-09-12 14:19:22 +0530

    feat: now users will get sentiment analysis every day with help of Scheduler

---
+++ journal-app-backend/src/main/java/net/LaabhGupta/journalApp/scheduler/UserScheduler.java
@@ -0,0 +1,55 @@
+package net.LaabhGupta.journalApp.scheduler;
+
+import net.LaabhGupta.journalApp.cache.AppCache;
+import net.LaabhGupta.journalApp.entity.JournalEntry;
+import net.LaabhGupta.journalApp.entity.User;
+import net.LaabhGupta.journalApp.repository.UserRepositoryImpl;
+import net.LaabhGupta.journalApp.service.EmailService;
+import net.LaabhGupta.journalApp.service.SentimentAnalysisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class UserScheduler {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Autowired
+    private SentimentAnalysisService sentimentAnalysisService;
+
+    @Autowired
+    private AppCache appCache;
+
+    @Scheduled(cron = "0 0 9 * * SUN")
+    public void fetchUsersAndSendSaMail(){
+        List<User> users = userRepository.getUserForSA();
+        for(User user: users){
+            List<JournalEntry> journalEntries = user.getJournalEntries();
+            List<String> filteredEntries = journalEntries.stream()
+                    .filter(
+                            x -> x.getDate().isAfter(
+                                    LocalDateTime.now().minus(7, ChronoUnit.DAYS))
+                    )
+                    .map(JournalEntry::getContent)
+                    .toList();
+            String entry = String.join(" ", filteredEntries);
+            String sentiment = sentimentAnalysisService.getSentiment(entry);
+            emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days", sentiment);
+        }
+    }
+
+    @Scheduled(cron = "0 0/10 * ? * *")
+    public void userAppCache(){
+        appCache.init();
+    }
+}
